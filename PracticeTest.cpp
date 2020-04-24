0bcdfe77e731415bc385dadbd24ea240bda63dfd
John Higdon <jmhigdon@gmail.com>
2020-04-23 17:27:26 -0700

    Added a few palindrome tests. Committing to test unit test functionality.

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -20,3 +20,36 @@ TEST(PracticeTest, is_simple_palindrome)
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_palindrome_with_mixed_case)
+{
+	Practice obj;
+	ASSERT_TRUE(obj.isPalindrome("Aa"));
+    ASSERT_TRUE(obj.isPalindrome("CiViC"));
+    ASSERT_TRUE(obj.isPalindrome("AnnA"));
+    ASSERT_TRUE(obj.isPalindrome("RacEcar"));
+}
+
+TEST(PracticeTest, is_palindrome_with_whitespace)
+{
+    Practice obj;
+    ASSERT_TRUE(obj.isPalindrome("race car"));
+    ASSERT_TRUE(obj.isPalindrome("my gym"));
+    ASSERT_TRUE(obj.isPalindrome("was it a cat i saw"));
+}
+
+TEST(PracticeTest, is_palindrom_with_whitespace_and_mixed_case)
+{
+    Practice obj;
+    ASSERT_TRUE(obj.isPalindrome("Race Car"));
+    ASSERT_TRUE(obj.isPalindrome("Was it a cat I saw"));
+    ASSERT_TRUE(obj.isPalindrome("Top spot"));
+}
+
+TEST(PracticeTest, is_palindrome_with_punctuation_whitespace_and_mixed_case)
+{
+    Practice obj;
+    ASSERT_TRUE(obj.isPalindrome("Was it a cat I saw?"));
+    ASSERT_TRUE(obj.isPalindrome("Eva, can I see bees in a cave?"));
+}
+
