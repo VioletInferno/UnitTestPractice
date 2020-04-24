cc0eba3cd172a61199470c0a0a93ba0e12a8a6a1
John Higdon <jmhigdon@gmail.com>
2020-04-23 19:26:15 -0700

    Attempt at fixing sortDescending bug which affected numbers already in ascending order.

--- Practice.cpp
+++ Practice.cpp
@@ -7,24 +7,24 @@ using std::string;
 // descending order from greatest (first) to least (third)
 void Practice::sortDescending(int & first, int & second, int & third)
 {
+    
   if( first < second )
   {
     int temp = first;
     first = second;
     second = temp;
   }
+  if( first < third )
+  {
+    int temp = first;
+    first = third;
+    third = temp;
+  }
   if( second < third )
   {
     int temp = second;
     second = third;
     third = temp;
-  
-    if( first < third )
-    {
-        int temp = first;
-        first = third;
-        third = temp;
-    }
   }
 }
 
